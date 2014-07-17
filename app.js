078382012cd04f22d9a8f73f31a91db5b3396e7a
tom-wr <tomwriggles@hotmail.com>
2014-07-17 19:01:18 +0100

    port fix

--- app.js
+++ app.js
@@ -11,6 +11,6 @@ io.on('connection', function(socket){
 	console.log('PIbot docking');
 })
 
-http.listen(3000, function(){
+http.listen(process.env.PORT || 5000, function(){
 	console.log('piNET is awake and listening on port %d', http.address().port);
 });
