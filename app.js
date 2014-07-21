752ea7e471b99ab1f3a19220653af5790988e80c
tom-wr <tomwriggles@hotmail.com>
2014-07-21 14:09:29 +0100

    docking data

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var wss =  require()
 
 /* GET home page. */
 app.get('/', function(req, res) {
@@ -8,12 +9,16 @@ app.get('/', function(req, res) {
 });
 
 io.on('connection', function(socket){
-	console.log('PIbot docking');
+	console.log('Client connected');
 	io.emit('order', {led:'on'});
 	setTimeout(function(){
 		io.emit('order', {led:'off'});
 	}, 5000)
 });
+
+io.on('dock', function(data){
+	console.log('piBot '+ data.name + 'is online.');
+})
 
 http.listen(process.env.PORT || 5000, function(){
 	console.log('piNET is awake and listening on port %d', http.address().port);
